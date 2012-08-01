5af869600ef8df5fc7c1936d554443664dc2217d
robbin <robbin@robbinmatoMacBook-Air.local>
2012-08-01 21:46:43 +0800

    微博OAuth2.0测试1

---
+++ libweibo/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '2232997713' );
+define( "WB_SKEY" , '2213ec0b885a8fa4e4858569cc70852e' );
+define( "WB_CALLBACK_URL" , 'https://github.com/lucky215/weibo/libweibo/callback.php' );
